e8d658c9222a40b57ed7a910160d178e8c9b9b45
Willem Essenstam <willem.essenst@nutanix.com>
2020-12-26 21:03:23 +0100

    Updated K8s yaml files

---
+++ Environment docker-compose files/create_env.sh
@@ -0,0 +1,10 @@
+mkdir -p /gitea/mysql
+mkdir -p /gitea/haproxy/ssl
+mkdir -p /gitea/docker_reg/data
+mkdir -p /gitea/drone/agent
+mkdir -p /gitea/drone/server
+mkdir -p /gitea/gitea
+mkdir -p /gitea/auth
+mkdir -p /gitea/nginx/ssl
+mkdir -p /gitea/nginx/conf.d
+chmod 777 -R /gitea
